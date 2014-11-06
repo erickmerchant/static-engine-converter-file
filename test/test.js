2794f6436ff82901c7b229a5a609f388b03261fd
Erick Merchant <erickmerchant@gmail.com>
2014-11-05 19:03:21 -0800

    Add tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var file = require('../index.js')();
+var assert = require('chai').assert;
+var moment = require('moment');
+
+describe('plugin', function(){
+
+    it('should add certain properties to the page object based on the file name', function(done){
+
+        file('category-a/2015-01-01.slug-a.txt', {}, function(file, page, next){
+
+            assert.deepEqual(page.category, 'category-a');
+
+            assert.deepEqual(page.date.format("YYYY-MM-DD"), '2015-01-01');
+
+            assert.deepEqual(page.slug, 'slug-a');
+
+            done();
+        });
+    });
+});
