0f80a9a6ab8ab5106ad23440b964e6b6503b6103
Erick Merchant <erickmerchant@gmail.com>
2015-04-10 23:36:34 -0700

    Adopt JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-var pathMatch = require('path-match')();
-var assign = require('object-assign');
+var pathMatch = require('path-match')()
+var assign = require('object-assign')
 
-module.exports = function(route, modifiers) {
+module.exports = function (route, modifiers) {
+  var matcher = pathMatch(route)
 
-    var matcher = pathMatch(route);
+  return function (pages, done) {
+    try {
+      pages = pages.map(function (page) {
+        var params = matcher(page.file)
+        var k
 
-    return function (pages, done) {
-
-        try
-        {
-            pages = pages.map(function(page){
-
-                var params = matcher(page.file);
-                var k;
-
-                for(k in params) {
-
-                    if(typeof modifiers[k] != 'undefined') {
-
-                        params[k] = modifiers[k](params[k]);
-                    }
-                }
-
-                return assign({}, page, params);
-            });
-        }
-        catch(e)
-        {
-            done(e)
+        for (k in params) {
+          if (typeof modifiers[k] !== 'undefined') {
+            params[k] = modifiers[k](params[k])
+          }
         }
 
-        done(null, pages);
-    };
+        return assign({}, page, params)
+      })
+    } catch(e) {
+      done(e)
+    }
+
+    done(null, pages)
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,41 +1,35 @@
-var file = require('../index.js');
-var assert = require('assert');
-var moment = require('moment');
+var file = require('../index.js')
+var assert = require('assert')
+var describe = require('mocha').describe
+var it = require('mocha').it
+var moment = require('moment')
 
 file = file(':categories+/:date.:slug.txt', {
-    date: function(date) {
+  date: function (date) {
+    return moment(date, 'YYYY-MM-DD')
+  }
+})
 
-        return moment(date, 'YYYY-MM-DD');
-    }
-});
+describe('plugin', function () {
+  it('should add certain properties to the page object based on the file name', function (done) {
+    file([{file: 'category-a/2015-01-01.slug-a.txt'}], function (err, pages) {
+      if (err) {
+        done(err)
+      } else {
+        pages[0].date = pages[0].date.format('YYYY-MM-DD')
 
-describe('plugin', function(){
+        assert.deepEqual(pages[0], {
+          categories: ['category-a'],
 
-    it('should add certain properties to the page object based on the file name', function(done){
+          file: 'category-a/2015-01-01.slug-a.txt',
 
-        file([{file: 'category-a/2015-01-01.slug-a.txt'}], function(err, pages){
+          date: '2015-01-01',
 
-            if(err) {
+          slug: 'slug-a'
+        })
 
-                done(err);
-            }
-            else {
-
-                pages[0].date = pages[0].date.format("YYYY-MM-DD");
-
-                assert.deepEqual(pages[0], {
-
-                    categories: ['category-a'],
-
-                    file: "category-a/2015-01-01.slug-a.txt",
-
-                    date: '2015-01-01',
-
-                    slug: 'slug-a',
-                });
-
-                done();
-            }
-        });
-    });
-});
+        done()
+      }
+    })
+  })
+})
