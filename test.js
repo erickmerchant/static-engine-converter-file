7023e1b3021db3eab05f7854fc34067bc62e5173
Erick Merchant <erickmerchant@gmail.com>
2015-04-12 00:08:27 -0700

    Move test

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-var file = require('../index.js')
+var plugin = require('./index.js')
 var assert = require('assert')
 var describe = require('mocha').describe
 var it = require('mocha').it
-var moment = require('moment')
+var file
 
-file = file(':categories+/:date.:slug.txt', {
+file = plugin(':categories+/:date.:slug.txt', {
   date: function (date) {
-    return moment(date, 'YYYY-MM-DD')
+    return date.split('-')
   }
 })
 
@@ -14,16 +14,14 @@ describe('plugin', function () {
   it('should add certain properties to the page object based on the file name', function (done) {
     file([{file: 'category-a/2015-01-01.slug-a.txt'}], function (err, pages) {
       if (err) {
-        done(err)
+        throw err
       } else {
-        pages[0].date = pages[0].date.format('YYYY-MM-DD')
-
         assert.deepEqual(pages[0], {
           categories: ['category-a'],
 
           file: 'category-a/2015-01-01.slug-a.txt',
 
-          date: '2015-01-01',
+          date: ['2015', '01', '01'],
 
           slug: 'slug-a'
         })
